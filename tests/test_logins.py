eef20ff5c465ba832fb9e70983cfec3c14837eba
Funky Brows <gravity_black@mailbox.org>
2023-02-20 17:48:18 -0500

    test: site w/ captcha

--- tests/test_logins.py
+++ tests/test_logins.py
@@ -3,6 +3,7 @@ import pytest
 from playwright.async_api import expect, Browser, Page
 
 from torrenting_logins.sites import torrent_leech
+from torrenting_logins.sites import the_geeks
 from torrenting_logins.login import login
 
 
@@ -13,3 +14,16 @@ async def test_tl(async_webkit):
 
     for category in ("Movies", "TV", "Games", "Apps", "Education"):
         expect(page.locator(f"//div[contains(text(), {category})]")).to_be_visible()
+
+
+@pytest.mark.asyncio
+async def test_needs_submit(async_chromium, async_firefox, async_webkit):
+    page = await login(
+        {
+            "chromium": await anext(async_chromium),
+            "firefox": await anext(async_firefox),
+            "webkit": await anext(async_webkit),
+        },
+        the_geeks,
+    )
+    await asyncio.sleep(1000)
